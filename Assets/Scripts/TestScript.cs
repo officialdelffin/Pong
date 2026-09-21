048548d949e73816f64f625848e8f0eea82b2cfe
oDelffin <workdelffin@gmail.com>
2026-09-21 18:08:03 -0300

    Testing the Start and Update funcions

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,14 +5,14 @@ public class TestScript : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
 
-
+        print("Camera started");
     
     }
 
     // Update is called once per frame
     void Update() {
 
-
+        print("Camera working");
         
     }
 
