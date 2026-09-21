a1e6c3426e8a562317ddc517a348aa63e81b72ce
oDelffin <workdelffin@gmail.com>
2026-09-21 19:31:13 -0300

    Running log tests

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 
+
 public class TestScript : MonoBehaviour {
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
 
+
         Debug.Log("A camera foi ligada!");
     
+
     }
+
 
     // Update is called once per frame
     void Update() {
 
-        Debug.Log("A camera está acompanhando o personagem!");
+
+        Debug.Log("A camera está gravando!");
+        Debug.LogWarning("A camera está descarregando!");
+        Debug.LogError("A camera descarregou!");
+
 
     }
+
 
 }
