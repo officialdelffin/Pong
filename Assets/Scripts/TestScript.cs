ceab9cd796770f47923b20fef379b3fa83fb5a20
oDelffin <workdelffin@gmail.com>
2026-09-21 18:00:46 -0300

    Just making a correction in formatation

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,25 +2,18 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour {
 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
 
 
-        
-
-
+    
     }
-
 
     // Update is called once per frame
     void Update() {
 
 
-
-
         
     }
-
 
 }
