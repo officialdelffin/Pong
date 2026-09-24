a810e99cb7ad5f231aa9a3fe358bbd4b776c577d
oDelffin <workdelffin@gmail.com>
2026-09-24 10:19:27 -0300

    Defining my racket's positions

--- Assets/Scripts/RacketControler.cs
+++ Assets/Scripts/RacketControler.cs
@@ -3,20 +3,31 @@ using UnityEngine;
 public class RacketControler : MonoBehaviour{
 
 
+    // Attributes :
+    private Vector3 movimentsPositions;
+    private float verticalPosition;  
+
+
     // Function that runs once when the game starts :
     void Start(){
 
 
+        // Defining the default position of the x and z axes :
+        movimentsPositions.x = -7.5f;
+        movimentsPositions.z = 0f;
 
 
-        
     }
 
     // Function that runs every frame :
     void Update(){
 
 
+        movimentsPositions.y = verticalPosition;
+        transform.position = movimentsPositions;
 
+
+        Debug.Log(transform.position.y);
 
         
     }
