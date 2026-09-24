94afed86b0dac5c26a0acafdb1d8d85929962a98
oDelffin <workdelffin@gmail.com>
2026-09-24 09:40:01 -0300

    Creating Racket sprite

---
+++ Assets/Scripts/RacketControler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class RacketControler : MonoBehaviour{
+
+
+    // Function that runs once when the game starts :
+    void Start(){
+
+
+
+
+        
+    }
+
+    // Function that runs every frame :
+    void Update(){
+
+
+
+
+        
+    }
+
+
+}
