2a02b19f725fc6067e8c58fe711a3c35a07e8ae4
oDelffin <workdelffin@gmail.com>
2026-09-25 09:23:26 -0300

    Correcting velocity to linearVelocity

--- Assets/Scripts/BallControler.cs
+++ Assets/Scripts/BallControler.cs
@@ -6,15 +6,21 @@ public class BallControler : MonoBehaviour{
 
     // Atributes :
     public Rigidbody2D rigidboryBall;
+    public Vector2 speedBall;
 
 
     // Function that runs once when the game starts :
     void Start(){
 
 
+        // Defining the initial position of the ball :
+        transform.position = new Vector3(0, 0, 0);
 
 
-        
+        // Defining the initial speed of the ball :
+        rigidboryBall.linearVelocity = speedBall;
+
+
     }
 
 
