6f7bf90c7b2ee98a36d07012057ee4b98f86a85b
oDelffin <workdelffin@gmail.com>
2026-09-25 07:57:52 -0300

    Creating the ball script

---
+++ Assets/Scripts/BallControler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class BallControler : MonoBehaviour{
+
+
+    // Atributes :
+    public Rigidbody2D rigidboryBall;
+
+
+    // Function that runs once when the game starts :
+    void Start(){
+
+
+
+
+        
+    }
+
+
+    // Function that runs every frame :
+    void Update(){
+
+
+
+
+        
+    }
+
+
+}
